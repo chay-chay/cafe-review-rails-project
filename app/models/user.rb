5f132c491ec79fc462bad3b402b219fbefbcd311
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-01-18 01:09:55 -0500

    Add signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :cafes, through: :reviews
     has_one :cafe #optional that they have created
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
