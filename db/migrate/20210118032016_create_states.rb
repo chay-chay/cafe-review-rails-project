009088a27a35a350a49dc1342680b4bc405e50fb
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 20:33:42 -0500

    change t.string to t.text for deploy on Heroku

--- db/migrate/20210118032016_create_states.rb
+++ db/migrate/20210118032016_create_states.rb
@@ -1,7 +1,7 @@
 class CreateStates < ActiveRecord::Migration[6.1]
   def change
     create_table :states do |t|
-      t.string :name
+      t.text :name
 
       t.timestamps
     end
--- db/migrate/20210118032343_create_shops.rb
+++ db/migrate/20210118032343_create_shops.rb
@@ -1,7 +1,7 @@
 class CreateShops < ActiveRecord::Migration[6.1]
   def change
     create_table :shops do |t|
-      t.string :name
+      t.text :name
       t.belongs_to :state
       t.belongs_to :user
 
