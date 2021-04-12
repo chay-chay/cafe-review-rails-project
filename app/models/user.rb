bd747c1a547bd6075c6e969f96416102be3528ef
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-12 19:32:04 -0400

    change attribute to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
     
     def self.from_omniauth(response)
         User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
-            u.username = response[:info][:username]
+            u.username = response[:info][:email]
             u.email = response[:info][:email]
             u.password = SecureRandom.hex(15)
         end
